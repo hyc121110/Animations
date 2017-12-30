df0cca31ad0e2e18b4d62d74140a962cef857cd1
spagabtti <spagabtti@gmail.com>
2017-12-29 17:00:00 -0800

    edit

--- app/src/main/java/com/spaga/animations/MainActivity.java
+++ app/src/main/java/com/spaga/animations/MainActivity.java
@@ -10,10 +10,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
         ImageView image1 = findViewById(R.id.image1);
-        ImageView image2 = findViewById(R.id.image2);
+        //ImageView image2 = findViewById(R.id.image2);
 
-        image1.animate().alpha(0f).setDuration(2000);
-        image2.animate().alpha(1f).setDuration(2000);
+        image1.animate().translationYBy(1000f).setDuration(2000);
+        //image1.animate().alpha(0f).setDuration(2000);
+        //image2.animate().alpha(1f).setDuration(2000);
     }
 
 
